3f5311f5f06a95e49cb4bde55b2a5dcc6990949a
jto05 <julian.to2023@gmail.com>
2026-07-11 12:04:47 -0700

    feat: implement ring buffer with wrap-around

--- internal/console/ringbuffer.go
+++ internal/console/ringbuffer.go
@@ -1,5 +1,44 @@
 package console
 
-// TODO: define RingBuffer struct
-// TODO: write NewRingBuffer(size int) *RingBuffer
-// TODO: write Push(line string) and Lines() []string (in order)
+type RingBuffer[T any] struct {
+	buffer []T
+	size   int
+	head   int
+	full   bool
+}
+
+func NewRingBuffer[T any](size int) *RingBuffer[T] {
+	rb := RingBuffer[T]{
+		buffer: make([]T, size),
+		size:   size,
+		head:   0,
+		full:   false,
+	}
+	return &rb
+}
+
+func (rb *RingBuffer[T]) Push(item T) {
+	rb.buffer[rb.head] = item
+
+	// increment head value and wrap when equals size
+	rb.head = (rb.head + 1) % rb.size
+	if !rb.full && rb.head == 0 {
+		rb.full = true
+	}
+}
+
+func (rb *RingBuffer[T]) ToSlice() []T {
+	var slice []T
+
+	if !rb.full {
+		slice = rb.buffer[:rb.head]
+		return slice
+	}
+
+	slice = make([]T, rb.size)
+	remainingIdx := rb.size - rb.head
+	copy(slice, rb.buffer[rb.head:])                // copy everything after the head into first half
+	copy(slice[remainingIdx:], rb.buffer[:rb.head]) // copy remaining before head into second half
+
+	return slice
+}
