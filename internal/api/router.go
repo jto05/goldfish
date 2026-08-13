240b2fcbd2233ca97ddb14f646f493ffb4eb5c2b
jto05 <julian.to2023@gmail.com>
2026-08-13 12:46:02 -0700

    feat: add router to route http requests to server handlers

--- internal/api/router.go
+++ internal/api/router.go
@@ -1,1 +1,27 @@
 package api
+
+import (
+	"net/http"
+
+	"github.com/jto05/goldfish/internal/console"
+	"github.com/jto05/goldfish/internal/process"
+)
+
+type Handler struct {
+	manager *process.Manager
+	hub     *console.Hub
+}
+
+func NewHandler(manager *process.Manager, hub *console.Hub) *Handler {
+	return &Handler{manager, hub}
+}
+
+func (h *Handler) Routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /api/server/status", h.serverStatus)
+	mux.HandleFunc("POST /api/server/start", h.serverStart)
+	mux.HandleFunc("POST /api/server/stop", h.serverStop)
+	mux.HandleFunc("POST /api/server/command", h.serverCommand)
+	return nil
+}
